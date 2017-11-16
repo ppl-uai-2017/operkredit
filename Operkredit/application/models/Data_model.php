2deab907412e59ea6fc0881792fa9747eacdb9ac
umiyati <umiyati1996@gmail.com>
2017-11-16 23:25:47 +0700

    update progress
    menambahkan model untuk transaksi

--- Operkredit/application/models/Data_model.php
+++ Operkredit/application/models/Data_model.php
@@ -101,4 +101,53 @@ class Data_model extends CI_Model
             return false;
         }
     }
+	
+	function getTransaksi()
+    {
+        $this->db->select('*');
+        $this->db->from('user a');
+		$this->db->join('pengunjung b', "a.email = b.email");
+        $this->db->join('pengambilan_kredit c', "c.username = a.username");
+		$this->db->join('rumah d', "c.id_barang = d.idrumah");
+        
+		
+        $query = $this->db->get();
+        if ($query->num_rows() != 0) {
+            return $query->result_array();
+        } else {
+            return false;
+        }
+    }
+	
+	function getDetailTransaksiPenerima($id)
+    {
+        $this->db->select('*');
+        $this->db->from('user a');
+		$this->db->join('pengunjung b', "a.email = b.email");
+        $this->db->join('pengambilan_kredit c', "c.username = a.username");
+		$this->db->join('rumah d', "c.id_barang = d.idrumah");
+        
+        $query = $this->db->get();
+        if ($query->num_rows() != 0) {
+            return $query->result_array();
+        } else {
+            return false;
+        }
+    }
+	
+	function getDetailTransaksiPengoper($id)
+    {
+        $this->db->select('*');
+        $this->db->from('pengambilan_kredit a');
+		$this->db->join('rumah b', "a.id_barang = b.idrumah");
+		$this->db->join('user d', "b.pengoper_kredit = d.username");
+		$this->db->join('pengunjung e', "e.email = d.email");
+        
+        $query = $this->db->get();
+        if ($query->num_rows() != 0) {
+            return $query->result_array();
+        } else {
+            return false;
+        }
+    }
 }
