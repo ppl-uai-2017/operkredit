43d3618f82a33d874feb82fadef03750c149162e
umiyati <umiyati1996@gmail.com>
2017-11-16 23:26:54 +0700

    upload progress
    detail transaksi

---
+++ Operkredit/application/views/pengesahan/detailTransaksi.php
@@ -0,0 +1,349 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="assets/img/favicon.ico">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <title>Light Bootstrap Dashboard by Creative Tim</title>
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+
+    <!-- Bootstrap core CSS     -->
+    <link href="<?php echo base_url("./assets/css/bootstrap.min.css")?>" rel="stylesheet" />
+
+    <!-- Animation library for notifications   -->
+    <link href="<?php echo base_url("./assets/css/animate.min.css")?>" rel="stylesheet"/>
+
+    <!--  Light Bootstrap Table core CSS    -->
+    <link href="<?php echo base_url("./assets/css/light-bootstrap-dashboard.css")?>" rel="stylesheet"/>
+
+
+    <!--  CSS for Demo Purpose, don't include it in your project     -->
+    <link href="<?php echo base_url("./assets/css/demo.css")?>" rel="stylesheet" />
+
+
+    <!--     Fonts and icons     -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300' rel='stylesheet' type='text/css'>
+    <link href="<?php echo base_url("./assets/css/pe-icon-7-stroke.css")?>" rel="stylesheet" />
+</head>
+<body>
+
+<div class="wrapper">
+    <div class="sidebar" data-color="black" >
+        <!--   you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple" -->
+        <div class="sidebar-wrapper">
+            <div class="logo">
+                <a href="#" class="simple-text">
+                    Operkredit
+                </a>
+            </div>
+
+            <ul class="nav">
+                <li>
+                    <a href="<?php echo base_url("index.php/pengesahan")?>">
+                        <i class="pe-7s-graph"></i>
+                        <p>Dashboard</p>
+                    </a>
+                </li>
+                <li>
+                    <a href="<?php echo base_url("index.php/pengesahan/customer")?>">
+                        <i class="pe-7s-user"></i>
+                        <p>Daftar Registrasi</p>
+                    </a>
+                </li>
+                <li class="active">
+                    <a href="<?php echo base_url("index.php/pengesahan/produk")?>">
+                        <i class="pe-7s-note2"></i>
+                        <p>Daftar Produk</p>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="<?php echo base_url("index.php/pengesahan/transaksi")?>">
+                        <i class="pe-7s-shopbag"></i>
+                        <p>Daftar Transaksi</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="main-panel">
+        <nav class="navbar navbar-default navbar-fixed">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Table List</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-left">
+                        <li>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-dashboard"></i>
+                                <p class="hidden-lg hidden-md">Dashboard</p>
+                            </a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="fa fa-globe"></i>
+                                <b class="caret hidden-sm hidden-xs"></b>
+                                <span class="notification hidden-sm hidden-xs">5</span>
+                                <p class="hidden-lg hidden-md">
+                                    5 Notifications
+                                    <b class="caret"></b>
+                                </p>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Notification 1</a></li>
+                                <li><a href="#">Notification 2</a></li>
+                                <li><a href="#">Notification 3</a></li>
+                                <li><a href="#">Notification 4</a></li>
+                                <li><a href="#">Another notification</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="">
+                                <i class="fa fa-search"></i>
+                                <p class="hidden-lg hidden-md">Search</p>
+                            </a>
+                        </li>
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="">
+                                <p>Account</p>
+                            </a>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <p>
+                                    Dropdown
+                                    <b class="caret"></b>
+                                </p>
+
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Separated link</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <p>Log out</p>
+                            </a>
+                        </li>
+                        <li class="separator hidden-lg hidden-md"></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title" align="center">Detail Transaksi User</h4>
+                                <hr>
+
+                                <div class="col-md-6">
+                                    <div class="card">
+                                        <table class="table table-hover table-striped">
+                                            <?php
+                                            foreach ($data as $data) {
+                                                ?>
+                                                <tr>
+                                                    <td>No. invoice : </td>
+                                                    <td><?php echo $data['id_pengambilan_kredit'] ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Tanggal Transaksi: </td>
+                                                    <td><?php echo $data['tanggal'] ?></td>
+                                                </tr>
+												<?php
+												foreach ($data1 as $data1) {
+													?>
+													<tr>
+														<td>From : </td>
+														<td><?php echo $data1['nama_lengkap'] ?></td>
+													</tr>
+													<?php
+												}?>
+                                                <tr>
+                                                    <td>For : </td>
+                                                    <td><?php echo $data['nama_lengkap'] ?></td>
+                                                </tr>
+												<tr>
+                                                    <td>Luas Bangunan : </td>
+                                                    <td><?php echo $data['luas_bangunan'] ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Kamar : </td>
+                                                    <td><?php echo $data['kamar'] ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Sertifikasi : </td>
+                                                    <td><?php echo $data['sertifikasi'] ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Kota : </td>
+                                                    <td><?php echo $data['kota'] ?></td>
+                                                </tr><tr>
+                                                    <td>Dokumen : </td>
+                                                    <td><?php echo $data['dokumen'] ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Alamat : </td>
+                                                    <td><?php echo $data['alamat'] ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td>Deskripsi : </td>
+                                                    <td><?php echo $data['deskripsi'] ?></td>
+                                                </tr>
+                                                <?php
+                                            }
+                                            ?>
+                                        </table>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-6">
+                                    <div class="card">
+                                        <table class="table table-hover table-striped">
+                                            <tr>
+                                                <td>Foto : </td>
+                                                <td><a href="<?php echo base_url("./rumah/".$data['foto1']) ?>"> <img src="<?php echo base_url("./rumah/".$data['foto1']) ?>" width="100"> </a> <br><br> <a href="<?php echo base_url("./rumah/".$data['foto1']) ?>"> <img src="<?php echo base_url("./rumah/".$data['foto2']) ?>" width="100"> </a>  <br><br> <a href="<?php echo base_url("./rumah/".$data['foto1']) ?>"> <img src="<?php echo base_url("./rumah/".$data['foto3']) ?>" width="100"> </a>  <br><br> <a href="<?php echo base_url("./rumah/".$data['foto1']) ?>"> <img src="<?php echo base_url("./rumah/".$data['foto4']) ?>" width="100"> </a> </td>
+                                            </tr>
+                                            <tr>
+                                                <td>Harga : </td>
+                                                <td><?php echo $data['harga'] ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Total Cicilan : </td>
+                                                <td><?php echo $data['total_cicilan'] ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Status Verifikasi Produk : </td>
+                                                <td><?php echo $data['status'] ?></td>
+                                            </tr>
+                                            <tr>
+                                                <td>Stok barang : </td>
+                                                <td><?php echo $data['stok'] ?></td>
+                                            </tr>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php
+                            if($data['verifikasi'] == "Disetujui")
+                            {
+                                ?>
+                                <a href="<?php echo base_url("index.php/pengesahan/deniedTransaksi/" . $data['id_pengambilan_kredit']) ?>" type="button" name="submit" rel="tooltip" title="Tolak"
+                                        class="btn btn-danger btn-simple btn-xs"
+                                        onclick="return confirm('Tolak Transaksi ini ?');">
+                                    <div class="btn btn-danger">Tolak Transaksi</div>
+                                </a>
+                            <?php
+                            }
+                            ?>
+
+                            <?php
+                            if($data['verifikasi'] == "Menunggu" || $data['verifikasi'] == "Ditolak")
+                            {
+                                ?>
+                                <a href="<?php echo base_url("index.php/pengesahan/approveTransaksi/" . $data['id_pengambilan_kredit']) ?>" type="button" name="submit" rel="tooltip" title="Verifikasi"
+                                    class="btn btn-info btn-simple btn-xs"
+                                    onclick="return confirm('Konfirmasi Transaksi ini ?')">
+                                <div class="btn btn-success">Setujui Verifikasi</div>
+                                </a>
+                                </form>
+                            <?php
+                            }
+                            ?>
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <footer class="footer">
+            <div class="container-fluid">
+                <nav class="pull-left">
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Company
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Portfolio
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                Blog
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <p class="copyright pull-right">
+                    &copy; <script>document.write(new Date().getFullYear())</script> <a href="http://www.creative-tim.com">Creative Tim</a>, made with love for a better web
+                </p>
+            </div>
+        </footer>
+
+
+    </div>
+</div>
+
+
+</body>
+
+<!--   Core JS Files   -->
+<script src="<?php echo base_url("./assets/js/jquery-1.10.2.js")?>" type="text/javascript"></script>
+<script src="<?php echo base_url("./assets/js/bootstrap.min.js")?>" type="text/javascript"></script>
+
+<!--  Checkbox, Radio & Switch Plugins -->
+<script src="<?php echo base_url("./assets/js/bootstrap-checkbox-radio-switch.js")?>"></script>
+
+<!--  Charts Plugin -->
+<script src="<?php echo base_url("./assets/js/chartist.min.js")?>"></script>
+
+<!--  Notifications Plugin    -->
+<script src="<?php echo base_url("./assets/js/bootstrap-notify.js")?>"></script>
+
+<!--  Google Maps Plugin    -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+<!-- Light Bootstrap Table Core javascript and methods for Demo purpose -->
+<script src="<?php echo base_url("./assets/js/light-bootstrap-dashboard.js")?>"></script>
+
+<!-- Light Bootstrap Table DEMO methods, don't include it in your project! -->
+<script src="<?php echo base_url("./assets/js/demo.js")?>"></script>
+
+</html>
